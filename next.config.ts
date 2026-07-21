cae20a2d57696123b16befa1ee405910f755e0d1
Jmail Filho <jamilf.alien@gmail.com>
2026-07-21 00:06:34 -0300

    feat: add Signal redirect

--- next.config.ts
+++ next.config.ts
@@ -13,9 +13,14 @@ const nextConfig: NextConfig = {
             destination: 'https://t.me/novasdecadamanha_devocional',
             permanent: true,
           },
+          {
+            source: '/devocional-no-signal',
+            destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
+            permanent: true,
+          },
         ]
     },
-    allowedDevOrigins: ["10.47.240.46","192.168.1.209","10.62.60.46","192.168.100.12", "192.168.1.97", "10.48.198.46"],
+    allowedDevOrigins: ["10.47.240.46","192.168.1.209","10.62.60.46","192.168.100.12", "192.168.1.97", "10.48.198.46", "192.168.18.7"],
 }
 
 export default nextConfig
