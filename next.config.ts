39cdac8970fd6c59c143d1918514ad674dee8179
Jmail Filho <jamilf.alien@gmail.com>
2026-07-22 10:44:45 -0300

    feat: adding redirections

--- next.config.ts
+++ next.config.ts
@@ -42,10 +42,19 @@ const nextConfig: NextConfig = {
             source: '/devocional-no-signal',
             destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
             permanent: true,
+          },{
+            source: '/devocional-signal',
+            destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
+            permanent: true,
+          },
+          {
+            source: '/devocional-para-signal',
+            destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
+            permanent: true,
           },
         ]
     },
-    allowedDevOrigins: ["10.47.240.46","192.168.1.209","10.62.60.46","192.168.100.12", "192.168.1.97", "10.48.198.46", "192.168.18.7"],
+    allowedDevOrigins: ["10.47.240.46","192.168.1.209","10.62.60.46","192.168.100.12", "192.168.1.97", "10.48.198.46", "192.168.18.7","192.168.100.11"],
 }
 
 export default nextConfig
