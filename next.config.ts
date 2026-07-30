bebf3c81023e37cbedb6d8d029524f0afc29ab63
Jmail Filho <jamilf.alien@gmail.com>
2026-07-29 22:31:55 -0300

    feat: update redirects

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,161 @@ const nextConfig: NextConfig = {
     async redirects() {
       return [
         {
+          source: '/private/:path*',
+          destination: '/',
+          permanent: true
+        },
+        {
+          source: '/members/:path*',
+          destination: '/',
+          permanent: true
+        },
+        {
+          source: '/ghost/:path*',
+          destination: '/',
+          permanent: true
+        },
+        {
+          source: '/fonts/:path*',
+          destination: '/',
+          permanent: true
+        },
+        {
+          source: '/rss-o-fruto-do-espirito',
+          destination: '/feed.rss',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito',
+          destination: '/series/o-fruto-do-espirito',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito',
+          destination: '/devocionais/dia-01-o-fruto-do-espirito',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-amor',
+          destination: '/devocionais/dia-02-o-fruto-do-espirito-e-amor',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-alegria',
+          destination: '/devocionais/dia-03-o-fruto-do-espirito-e-alegria',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-paz',
+          destination: '/devocionais/dia-04-o-fruto-do-espirito-e-paz',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-paciencia',
+          destination: '/devocionais/dia-05-o-fruto-do-espirito-e-paciencia',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-amabilidade',
+          destination: '/devocionais/dia-06-o-fruto-do-espirito-e-amabilidade',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-bondade',
+          destination: '/devocionais/dia-07-o-fruto-do-espirito-e-bondade',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/devocional-07-o-fruto-do-espirito-bondade',
+          destination: '/devocionais/dia-07-o-fruto-do-espirito-e-bondade',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-fidelidade',
+          destination: '/devocionais/dia-08-o-fruto-do-espirito-e-fidelidade',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-mansidao',
+          destination: '/devocionais/dia-09-o-fruto-do-espirito-e-mansidao',
+          permanent: true
+        },
+        {
+          source: '/o-fruto-do-espirito/o-fruto-do-espirito-dominio-proprio',
+          destination: '/devocionais/dia-10-o-fruto-do-espirito-e-dominio',
+          permanent: true
+        },
+        {
+          source: '/rss-salmo-119',
+          destination: '/feed.rss',
+          permanent: true
+        },
+        {
+          source: '/salmos',
+          destination: '/biblia/salmos',
+          permanent: true
+        },
+        {
+          source: '/salmos/page/2',
+          destination: '/biblia/salmos?page=2',
+          permanent: true
+        },
+        {
+          source: '/santidade',
+          destination: '/temas/santidade',
+          permanent: true
+        },
+        {
+          source: '/santificacao',
+          destination: '/temas/santificacao',
+          permanent: true
+        },
+        {
+          source: '/salvacao',
+          destination: '/temas/salvacao',
+          permanent: true
+        },
+        {
+          source: '/secreto',
+          destination: '/temas/secreto',
+          permanent: true
+        },
+        {
+          source: '/seguranca',
+          destination: '/temas/seguranca',
+          permanent: true
+        },
+        {
+          source: '/soberania',
+          destination: '/temas/soberania',
+          permanent: true
+        },
+        {
+          source: '/sofrimento',
+          destination: '/temas/sofrimento',
+          permanent: true
+        },
+        {
+          source: '/submissao',
+          destination: '/temas/submissao',
+          permanent: true
+        },
+        {
+          source: '/temor',
+          destination: '/temas/temor',
+          permanent: true
+        },
+        {
+          source: '/tempo-ordinario-links',
+          destination: '/links',
+          permanent: true
+        },
+        {
+          source: '/sermao-do-monte-links',
+          destination: '/links',
+          permanent: true
+        },
+        {
           source: '/dietrich-bonhoeffer-discipulado',
           destination: 'https://link.amazon/B0eVpMVUS',
           permanent: true
@@ -16,6 +171,11 @@ const nextConfig: NextConfig = {
         {
           source: '/sermao-do-monte',
           destination: '/series/sermao-do-monte',
+          permanent: true
+        },
+        {
+          source: '/sermao-do-monte/:path*',
+          destination: '/devocionais/:path*',
           permanent: true
         },
         {
@@ -124,6 +284,11 @@ const nextConfig: NextConfig = {
           permanent: true
         },
         {
+          source: '/tempo-ordinario',
+          destination: '/series/tempo-ordinario',
+          permanent: true
+        },
+        {
           source: '/tempo-ordinario/:path*',
           destination: '/devocionais/:path*',
           permanent: true
@@ -159,13 +324,28 @@ const nextConfig: NextConfig = {
           permanent: true,
         },
         {
+          source: '/devocional/whatsapp',
+          destination: 'https://whatsapp.com/channel/0029Vb7awlL8aKvAzcgyrd3c',
+          permanent: true,
+        },
+        {
           source: '/devocional-para-whatsapp',
+          destination: 'https://whatsapp.com/channel/0029Vb7awlL8aKvAzcgyrd3c',
+          permanent: true,
+        },
+        {
+          source: '/devocional-diario-para-whatsapp',
           destination: 'https://whatsapp.com/channel/0029Vb7awlL8aKvAzcgyrd3c',
           permanent: true,
         },
         {
           source: '/devocional-whatsapp',
           destination: 'https://whatsapp.com/channel/0029Vb7awlL8aKvAzcgyrd3c',
+          permanent: true,
+        },
+        {
+          source: '/devocional/telegram',
+          destination: 'https://t.me/novasdecadamanha_devocional',
           permanent: true,
         },
         {
@@ -195,6 +375,51 @@ const nextConfig: NextConfig = {
         {
           source: '/devocional-para-signal',
           destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
+          permanent: true,
+        },
+        {
+          source: '/devocional/signal',
+          destination: 'https://signal.group/#CjQKIG0RkZIwAML4qk3DeEimEKf8nRmHbyY8PKX5mNaVnSiSEhBG9rXeMMrWV4j9Fd_iaNK2',
+          permanent: true,
+        },
+        {
+          source: '/devocional-do-dia',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/devocional-curto',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/devocional',
+          destination: '/devocionais',
+          permanent: true,
+        },
+        {
+          source: '/devocional-da-manha',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/devocional-diario',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/devocional-diario-2021',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/devocional-diario-2021/:path*',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/content/:path*',
+          destination: '/',
           permanent: true,
         },
       ]
